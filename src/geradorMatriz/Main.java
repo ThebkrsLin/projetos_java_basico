9e92d4bc2c1a070ec0762d9caa5a3aa0ecbabac9
Cauã Carlos <93791778+ThebkrsLin@users.noreply.github.com>
2026-05-12 17:20:50 -0300

    overall matrix-generator base done

--- src/geradorMatriz/Main.java
+++ src/geradorMatriz/Main.java
@@ -1,11 +1,13 @@
 package geradorMatriz;
 
 import java.util.InputMismatchException;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String args[]) {
 		Scanner input = new Scanner(System.in);
+		Random random = new Random();
 		int addL = 0;
 		int addC = 0;
 
@@ -15,6 +17,7 @@ public class Main {
 				addL = input.nextInt();
 				System.out.print("Digite a quantidade de colunas da matriz: ");
 				addC = input.nextInt();
+				input.nextLine();
 				if (addC < 0 || addL < 0) {
 					System.out
 							.println("Os valores das linhas e das colunas tem que ser maior que 0, tente novamente!!");
@@ -30,13 +33,33 @@ public class Main {
 
 		int[][] matrix = new int[addL][addC];
 
-		System.out.println("Lines: " + addL + "Coluns: " + addC);
-		for (int l = 0; l < addL; l++) {
-			for (int c = 0; c < addC; c++) {
-				System.out.print("Digite o Valor na posição [ " + (l + 1) + ", " + (c + 1) + " ]: ");
-				matrix[l][c] = input.nextInt();
-				// input.nextInt();
+		while (true) {
+			System.out.println("Quer Gerar os valores da matrix Automaticamente?[S/N]: ");
+			String choose = input.nextLine();
+			// input.nextLine();
+
+			if (choose.toUpperCase().equalsIgnoreCase("S")) {
+				for (int l = 0; l < addL; l++) {
+					for (int c = 0; c < addC; c++) {
+						matrix[l][c] = random.nextInt(100);
+					}
+				}
 			}
+
+			else if (choose.toUpperCase().equalsIgnoreCase("N")) {
+				for (int l = 0; l < addL; l++) {
+					for (int c = 0; c < addC; c++) {
+						System.out.print("Digite o Valor na posição [ " + (l + 1) + ", " + (c + 1) + " ]: ");
+						matrix[l][c] = input.nextInt();
+					}
+				}
+			}
+
+			else {
+				System.out.println("Opção Inválida, tente novamente!!!!");
+				continue;
+			}
+			break;
 		}
 
 		for (int line = 0; line < addL; line++) {
